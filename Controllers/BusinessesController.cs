a2527e6404f25a20e8e9055a024647260e9d98e7
ShanaySharif <Shanay.uw17@gmail.com>
2023-08-18 18:48:52 -0400

    Add database context

--- Controllers/BusinessesController.cs
+++ Controllers/BusinessesController.cs
@@ -1,8 +1,59 @@
-
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using BusinessAPI.Models;
 
 
 
 namespace BusinessAPI.Controllers
 {
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessesController : ControllerBase
+    {
+    private readonly BusinessAPIContext _db;
+
+        public BusinessesController(BusinessAPIContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async
+        Task<ActionResult<IEnumerable<Business>>> Get (int businessId, string ResturantName, string shopName, string review, int rating )
+        {
+            IQueryable<Business> query = _db.Businesses.AsQueryable();
+
+            if (resturantName != null)
+            {
+                query = query.Where(entry => entry.ResturantName == resturantName);
+            }
+             if (locationName != null)
+
+            {
+               query = query.Where(entry => entry.LocationName = locationName);
+             }
+             if (shopName != null)
+             {
+                query = query.Where(entry => entry.ShopName == shopName);
+             }
+             if (rating > 0)
+             {
+                query = query.Where(entry => entry.Rating == rating);
+             }
+             var businesses = await query.ToListAsync();
+             if (busineses.Count == 0)
+             {
+                return NotFound();
+             }
+             return Ok(businesses);
+        }
+
+
+    }
 
 }
--- Models/BusinessAPIContext.cs
+++ Models/BusinessAPIContext.cs
@@ -5,7 +5,7 @@ namespace BuisnessAPI.Models
 {
   public class BuisnessAPIContext : DbContext
   {
-    public DbSet<Business> Business { get; set; }
+    public DbSet<Business> Businesses { get; set; }
 
     public BuisnessAPIContext(DbContextOptions<BuisnessAPIContext> options) : base(options)
     {
@@ -13,6 +13,49 @@ namespace BuisnessAPI.Models
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+      builder.Entity<Business>()
+        .HasData(
+          new Business { BusinessId = 1, RestaurantName = "Fox in the Snow Cafe", LocationName = "New Albany", Review = "Best Cafe Art in town", Rating = 10}, 
+
+          new Business { BusinessId = 2, RestaurantName = "Kitchen Social", LocationName = "WesterVille", Review = "Amazing chicken and waffles", Rating = 9.5}, 
+
+          new Business { BusinessId = 3, RestaurantName = "Kitchen Social", LocationName = "Dublin", Review = "Amazing chicken and waffles", Rating = 9.5}, 
+
+          new Business { BusinessId = 4, RestaurantName = "Zencha Cafe", LocationName = "High St", Review = "Masala Chai waffles were the lightest crispiest waffles ever chicken dumping looked ordinary but tasted heavenly", Rating = 8.9}, 
+
+
+          new Business { BusinessId = 5, RestaurantName = "Mozart's", LocationName = "Columbus", Review = "Authentic Tea Experience in Columbus", Rating = 3},
+
+
+           new Business { BusinessId = 6, RestaurantName = " Northstar Café ", LocationName = "TownsFair Way", Review = "This place has some of the best healthy food that I have ever tasted.", Rating = 10},
+
+           new Business { BusinessId = 7, RestaurantName = " Mitchell's Ocean Club", LocationName = "Easton Station", Review = "Best Seafood in town", Rating = 9},
+
+           new Business { BusinessId = 8, RestaurantName = " The Morning Ritual", LocationName = "Gahanna,", Review = "Great coffee & pastries, baked apple cinnamon roll is a must!", Rating = 7},
+
+
+           new Business { BusinessId = 9, RestaurantName = " Java Central Café", LocationName = "Westerville", Review = "Best cold brew nitro in town! Everything is awesome, especially the people.", Rating = 6},
+
+
+           new Business { BusinessId = 10, RestaurantName = " Safari Coffee House", LocationName = "Cleavland", Review = "One of the best Somali restaurants In Columbus", Rating = 10},
+
+
+           new Business { BusinessId = 11, ShopName = "Tigertree", LocationName = "Kossuth St", Review = "Popular mall. Good selection of stores. Clean.", Rating = 8},
+
+           new Business { BusinessId = 12, ShopName = "Lennox Town Center", LocationName = "Olenrangy River Rd", Review = "Nice shops, but the traffic and parking are awful.", Rating = 4},
+
+           new Business { BusinessId = 13, ShopName = "Vernacular - Grandview", LocationName = "Grandview Ave", Review = "Fantastic assortment of unique pieces, and a friendly group of associates!", Rating = 6},
+
+
+           new Business { BusinessId = 14, ShopName = "Heritage Square Antique Mall", LocationName = "Channingway Center Dr", Review = "Large antique mall with many different sellers and reasonable pricing.", Rating = 7},
+
+           new Business { BusinessId = 15, ShopName = "North Market Downtown", LocationName = "Grandview Ave,", Review = "Upscale market. Huge variety.", Rating = 5}
+
+
+
+
+
+        );
 
 
 
