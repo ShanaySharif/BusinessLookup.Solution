2e70a7f9fa0291489ab39043cdfff5827746de95
ShanaySharif <Shanay.uw17@gmail.com>
2023-08-18 20:24:47 -0400

    Add the route for the PUT action/id parameter

--- Controllers/BusinessesController.cs
+++ Controllers/BusinessesController.cs
@@ -82,6 +82,40 @@ public async Task<ActionResult<IEnumerable<Business>>> Get(int businessId, strin
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+
+      _db.Businesses.Update(business);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool BusinessExists(int id)
+    {
+      return _db.Businesses.Any(e => e.BusinessId == id);
+    }
  
 
 
